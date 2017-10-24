ec5841bd57c3a338a9bc2b2cb81fbec040a7f0bb
Ramjan Sk <30544406+ramjansk@users.noreply.github.com>
2017-10-24 18:51:56 +0530

    VowelsFind

---
+++ VowelsFind.java
@@ -0,0 +1,36 @@
+ import java.util.*;
+public class VowelsFind {
+ 
+public static void main(String a[]) {
+  
+System.out.println("Please enter a String");
+Scanner in = new Scanner(System.in);                  
+ String input = in.nextLine();
+ 
+     char[] Chararray = input.toCharArray();
+ 
+ int count = 0;
+ 
+ for (char ch : Chararray) {
+      switch (ch) {
+             case 'a':
+             case 'e':
+             case 'i':
+             case 'o':
+             case 'u':
+             case 'A':
+             case 'E':
+             case 'I':
+             case 'O':
+             case 'U':
+                 count++;
+     break;
+     default:
+            
+     }
+ }
+       System.out.println("Number of vowels in String "+count);
+ 
+}
+
+}
