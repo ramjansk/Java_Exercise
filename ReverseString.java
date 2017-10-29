a9d4f9a89d0b6f721fd0793687fb47279f8ecb16
Ramjan Sk <30544406+ramjansk@users.noreply.github.com>
2017-10-30 01:22:20 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,22 +1,3 @@
-/*import java.util.*;
- public class ReverseString { 
- public static void main(String args[]){
- String str; 
- String rStr; 
- Scanner bf=new Scanner(System.in);
- //input an integer number
- System.out.print("Enter any string: ");
- str=bf.nextLine();
- //Reversing String 
- rStr=;
- for(int loop=str.length()-1;loop>=0;loop--)
- rStr= rStr + str.charAt(loop); 
- System.out.println("Reversed string is: " + rStr);
- }
- }
-*/
-
-
  //java program to ReverseString using StringBuilder
 import java.lang.*;
 import java.io.*;
